55345b19b91b29405600d7b72e80700b68f7b8bb
Amro192 <amro2003.tariq@gmail.com>
2021-07-14 15:00:18 +0300

    prop edited

--- React/Class Components/PropUp/prop/src/App.js
+++ React/Class Components/PropUp/prop/src/App.js
@@ -5,10 +5,10 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName="Doe" lastName="Jane" age="45" hairColor="Black"/>
-      <PersonCard firstName="Smith" lastName="John" age="88" hairColor="Brown"/>
-      <PersonCard firstName="Amro" lastName="Othman" age="50" hairColor="Brown"/>
-      <PersonCard firstName="Smith" lastName="Maria" age="62" hairColor="Brown"/> 
+      <PersonCard firstName="Doe" lastName="Jane" age={20} hairColor="Black"/>
+      <PersonCard firstName="Smith" lastName="John" age={49} hairColor="Brown"/>
+      <PersonCard firstName="Amro" lastName="Othman" age={80} hairColor="Brown"/>
+      <PersonCard firstName="Smith" lastName="Maria" age={68} hairColor="Brown"/> 
     </div>
   );
 }
--- React/Class Components/PropUp/prop/src/components/PersonCard.js
+++ React/Class Components/PropUp/prop/src/components/PersonCard.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 class PersonCard extends Component {
-    render() {
+        constructor(props) {
+            super(props);
+            this.state={
+                age : this.props.age
+            };
+        }
+
+        render() {
+            const{firstName, lastName, age ,hairColor} = this.props;
         return (
             <div>
-                <h1>{this.props.firstName}, {this.props.lastName}</h1>
-                <h3>Age: {this.props.age}</h3>
-                <h3>Hair Color: {this.props.hairColor}</h3>
+                <h1>{firstName}, {lastName}</h1>
+                <h3>Age: {this.state.age}</h3>
+                <h3>Hair Color: {hairColor}</h3>
+                <button onClick={this.plusAge }>Add one</button>
             </div>
         )
+    }
+    plusAge = () => {
+        this.setState({age: this.state.age + 1 })
     }
 }
 
